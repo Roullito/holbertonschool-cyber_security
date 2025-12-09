005a2cb18f623d473ee934d152e6060337ffccb1
Roullito <jmoleins@laposte.net>
2025-12-09 04:38:07 -0500

    Update whois script to include additional fields in output for comprehensive CSV formatting

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' '/^(Registrant|Admin|Tech) /{f=$1;v=$2;if(f~/Ext$/)f=f":";if(f~/Street$/)v=v" ";print f","v}' > $1.csv
+whois $1 | awk -F': ' '/^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Fax|Email|Phone Ext|Fax Ext):/ {f=$1;v=$2;if(f~/Ext$/)f=f":";if(f~/Street$/)v=v" ";print f","v}' > $1.csv
