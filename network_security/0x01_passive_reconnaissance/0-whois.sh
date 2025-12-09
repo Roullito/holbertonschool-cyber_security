473c559d948017040969e0a0c0a165d7352ade81
Roullito <jmoleins@laposte.net>
2025-12-09 04:26:03 -0500

    Fix awk command in whois script and update CSV formatting for consistency

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1|awk -F': ' '/^(Registrant|Admin|Tech) /{gsub(/"/, "\"\"", $2); print $1 ",\"" $2 "\""}' >$1.csv
+whois $1|awk -F': ' '/^Registrant |^Admin |^Tech /{gsub(/^Registry /,"");print $1","$2}'>$1.csv
