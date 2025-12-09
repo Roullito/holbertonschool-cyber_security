8512c0e0b407236fc45320cc3fddccf83dd01193
Roullito <jmoleins@laposte.net>
2025-12-09 04:28:24 -0500

    Refactor whois script for improved output formatting and consistency in CSV files

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1|awk -F': ' '/^Registrant |^Admin |^Tech /{gsub(/^Registry /,"");print $1","$2}'>$1.csv
+whois $1 | awk -F': ' '/^(Registrant|Admin|Tech) /{f=$1;v=$2;if(f~/Ext$/)f=f":";if(f~/Street$/)v=v" ";print f","v}' > $1.csv
