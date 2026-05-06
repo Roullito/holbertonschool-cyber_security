2734006d25e2a5b762dca88cfe7a9ea13957e735
Jules Moleins <157913521+Roullito@users.noreply.github.com>
2026-05-06 11:53:25 +0200

    Update 4-write_file.rb

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,15 +1,17 @@
 #!/usr/bin/env ruby
 
-require 'json'
+require "json"
 
 def merge_json_files(file1_path, file2_path)
-  file1_content = File.read(file1_path)
-  file2_content = File.read(file2_path)
+    file1_content = File.read(file1_path)
+    file2_content = File.read(file2_path)
 
-  file1_data = JSON.parse(file1_content)
-  file2_data = JSON.parse(file2_content)
+    arr1 = JSON.parse(file1_content)
+    arr2 = JSON.parse(file2_content)
 
-  merged_data = file2_data + file1_data
+    merged = arr2 + arr1
 
-  File.write(file2_path, JSON.pretty_generate(merged_data))
+    File.write(file2_path, JSON.pretty_generate(merged))
+
+    puts "Merged JSON written to #{file2_path}"
 end
